f1fcecbaa0095ea24d1d91f94e32de0d211aa23f
Yuri <ygonzoo@gmail.com>
2022-03-11 12:09:03 +0100

    Actualizado index.js para utilizar async/await.

--- index.js
+++ index.js
@@ -1,39 +1,47 @@
+
 require('dotenv').config();
 const express = require('express')
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 const app = express()
 const port = 3000
 
-app.get('/api/', (req, res) => {
+app.get('/api/', async (req, res) => {
   // let user = req.query.user;
   // let app = req.query.app;
 
   let { user, app } = req.query;
 
-  const connection = mysql.createConnection(process.env.DATABASE_URL);
+  const connection = await mysql.createConnection(process.env.DATABASE_URL);
 
-  // QUERY!
-  /**
-   * Los valores de las consultas deben ser escapados para evitar inyección de SQL al escribir la query.
-   * Para escapar los valores podemos utilizar ?
-   * ver: https://github.com/mysqljs/mysql#escaping-query-values
-   * Para escapar los identificadores (nombres de tabla, campos, etc) podemos utilizar ??.
-   * ver: https://github.com/mysqljs/mysql#escaping-query-identifiers
-   */
-  connection.query(
-    'SELECT * FROM ?? WHERE user = ?',
-    [app, user],
-    function (err, results, fields) {
-      if (err) return res.json({ error: err });
-      if (results.length === 0) return res.json({ mensaje: "el usuario no tiene flags" })
-      let elem = results[0]
+  try {
+    // QUERY!
+    /**
+     * Los valores de las consultas deben ser escapados para evitar inyección de SQL al escribir la query.
+     * Para escapar los valores podemos utilizar ?
+     * ver: https://github.com/mysqljs/mysql#escaping-query-values
+     * Para escapar los identificadores (nombres de tabla, campos, etc) podemos utilizar ??.
+     * ver: https://github.com/mysqljs/mysql#escaping-query-identifiers
+     */
+  
+    // Utilizando mysql2/promise que nos envuelve a promesa y nos devuelve [rows, fields] como resultado
+    // y para capturar errores utilizamos catch.
+    const queryResult = await connection.query('SELECT * FROM ?? WHERE user = ?', [app, user]);
+    const [results, fields] = queryResult;
+    if (results.length === 0) throw ("el usuario no tiene flags")
 
-      res.json({
-        flag1: elem.flag1,
-        flag2: elem.flag2
-      })
+    // el primer elemento es el resultado de la query
+    let elem = results[0]
+
+    result = {
+      flag1: elem.flag1,
+      flag2: elem.flag2
     }
-  );
+
+    res.json(result)
+
+  } catch (err) {
+    res.json({ error: err })
+  }
 
   connection.end();
 })
@@ -41,12 +49,3 @@ app.get('/api/', (req, res) => {
 app.listen(port, () => {
   console.log(`http://localhost:${port}/api/?user=1&app=app1`)
 })
-
-
-
-
-
-
-
-
-
