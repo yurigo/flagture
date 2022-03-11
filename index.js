1794521ab0f877f835eee4061f72b4d106cdf00a
Yuri <ygonzoo@gmail.com>
2022-03-11 10:54:18 +0100

    🐛 using '??' to escape query identifiers (comments added)

--- index.js
+++ index.js
@@ -5,34 +5,37 @@ const app = express()
 const port = 3000
 
 app.get('/api/', (req, res) => {
-    // let user = req.query.user;
-    // let app = req.query.app;
+  // let user = req.query.user;
+  // let app = req.query.app;
 
-    let { user , app } = req.query;
+  let { user, app } = req.query;
 
-    const connection = mysql.createConnection(process.env.DATABASE_URL);
-    // simple query
-    connection.query(
-        'SELECT * FROM ? WHERE user = ?',
-        [app, user],
-        function(err, results, fields) {
-            //console.log(results); // results contains rows returned by server
-            //return console.log(err)
-            if (err) return res.json({error: err});
-            if (results.length === 0) return res.json({mensaje: "el usuario no tiene flags"})
+  const connection = mysql.createConnection(process.env.DATABASE_URL);
 
-            let elem = results[0]
+  // QUERY!
+  /**
+   * Los valores de las consultas deben ser escapados para evitar inyección de SQL al escribir la query.
+   * Para escapar los valores podemos utilizar ?
+   * ver: https://github.com/mysqljs/mysql#escaping-query-values
+   * Para escapar los identificadores (nombres de tabla, campos, etc) podemos utilizar ??.
+   * ver: https://github.com/mysqljs/mysql#escaping-query-identifiers
+   */
+  connection.query(
+    'SELECT * FROM ?? WHERE user = ?',
+    [app, user],
+    function (err, results, fields) {
+      if (err) return res.json({ error: err });
+      if (results.length === 0) return res.json({ mensaje: "el usuario no tiene flags" })
+      let elem = results[0]
 
-          res.json({
-            flag1: elem.flag1,
-            flag2: elem.flag2
-        })
-        }
-      );
-    
-    connection.end();
+      res.json({
+        flag1: elem.flag1,
+        flag2: elem.flag2
+      })
+    }
+  );
 
-  
+  connection.end();
 })
 
 app.listen(port, () => {
